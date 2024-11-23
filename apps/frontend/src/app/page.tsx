351048f904107d834e4055e41014e0e8d7181a2b
CarlosH505 <carlinhoshenrique505@gmail.com>
2024-11-23 11:11:06 -0300

    functions

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,10 @@
 import Pagina from "@/components/template/Pagina";
+import Evento from "@/core";
 
 export default function Home() {
+
+  const e: Evento
+
   return (
    <Pagina className="w-full">
     <div>Inicio</div>
